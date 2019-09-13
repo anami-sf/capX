8654c453d6deb3454e3d8bac22d4fdf1fc070dea
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 15:07:39 -0700

    completes login route and template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/', include('django.contrib.auth.urls')), 
     path('orders/', views.order_index, name='order_index'),
     # path('orders/<int:pk>/', views.OrderDetail.as_view(), name='order_detail'),
 ]
