b9d2e49fb2fe05f8a8700eeb4ed2898f93f84ae9
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 14:27:38 -0700

    creates admin and first superuser

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
+    # path('accounts/', include('django.contrib.auth.urls')), //for authentication
     path('orders/', views.order_index, name='order_index'),
     # path('orders/<int:pk>/', views.OrderDetail.as_view(), name='order_detail'),
-}
+]
