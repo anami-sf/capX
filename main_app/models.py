1f9b3d7c6c451233a85171a7a8d2f5036d088a69
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 14:03:15 -0700

    create Order model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 from django.urls import reverse 
 
+
+ORDER_TYPE_CHOICES = [
+    ('Bid', 'Bid'),
+    ('Ask', 'Ask')
+]
+
+ORDER_COIN_CHOICES = [
+    ('BTC', 'Bitcoin'),
+    ('ETH', 'Ethereum')
+
+]
+
+class Order:
+    amount= models.DecimalField(
+        max_digits=30,
+        decimal_places = 8
+    )
+    order_type= models.CharField(
+        max_length = 3,
+        choices = ORDER_TYPE_CHOICES,
+
+    )
+    coin_type= models.CharField(
+        max_length = 3,
+        choices = ORDER_COIN_CHOICES,
+       
+    )
+    # transaction = models.ForeignKey // 
