dc44ec5e8e2a7b22c61c3e0c2c514c1f3f1c74ca
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 14:32:43 -0700

    create route for user auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('accounts/', include('django.contrib.auth.urls')), //for authentication
+    path('accounts/', include('django.contrib.auth.urls')), 
     path('orders/', views.order_index, name='order_index'),
     # path('orders/<int:pk>/', views.OrderDetail.as_view(), name='order_detail'),
 ]
