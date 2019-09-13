4e45926446345300c6d2dd221a36b24e9f78e0d9
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 13:30:56 -0700

    setup boilerplate MVT for orders/index and orders/detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = {
     path('', views.home, name='home'),
+    path('orders/', views.order_index, name='order_index'),
+    # path('orders/<int:pk>/', views.OrderDetail.as_view(), name='order_detail'),
 }
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 def home(request):
     return render (request,'home.html')
+
+def order_index(request):
+    return render (request,'orders/index.html')
+
+def order_detail(request):
+    return render (request,'orders/detail.html')
+
+
