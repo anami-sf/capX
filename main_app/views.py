4c4ffdacab3dbfccb3d00319ac4d89820fac5f32
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-09-13 16:11:50 -0700

    create view for login

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def home(request):
@@ -12,5 +15,24 @@ def order_index(request):
 
 def order_detail(request):
     return render (request,'orders/detail.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+        # This will add the user to the database
+            user = form.save()
+        # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
